1236cd737cd8f8962f3d1e4e493839ae55ff931f
Janne Julkunen <janne.julkunen@zef.fi>
2016-08-30 15:09:08 +0300

    Added angular2 template loader

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
       {
     	  test: /\.ts$/,
     	  loaders: [
-					'awesome-typescript-loader'
+					'awesome-typescript-loader',
+					'angular2-template-loader'
         ]
     	},
       {
