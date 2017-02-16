ba6b053ef193ff87a6c9e3fa46614378c15b9612
Janne Julkunen <janne.julkunen@zef.fi>
2017-02-16 16:05:59 +0200

    Added systemjs build system for the example app

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Font } from 'angular2-font-picker';
 
 @Component({
+  moduleId: module.id + '',
   selector: 'my-app',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
