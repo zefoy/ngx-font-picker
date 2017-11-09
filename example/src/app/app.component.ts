31a8ee8bf3415a86ece37b8c11ee621f888fc087
Janne Julkunen <janne.julkunen@zef.fi>
2017-11-09 15:12:58 +0200

    Small fix to make the example work with systemjs

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 import { Font } from 'ngx-font-picker';
 
 @Component({
-  moduleId: module.id + '',
   selector: 'my-app',
+  moduleId: 'src/app/app.component',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
